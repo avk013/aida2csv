f9801526839c8fe0e79a211a5e71f3030eb55401
Ol Kovalenko <avk013@gmail.com>
2021-06-05 12:22:13 +0300

    настоящая beta2

--- aida2csv/Form1.Designer.cs
+++ aida2csv/Form1.Designer.cs
@@ -30,35 +30,46 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(109, 238);
+            this.button1.Location = new System.Drawing.Point(5, 196);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(149, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "select AIDA report files";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 37);
+            this.textBox1.Location = new System.Drawing.Point(5, 24);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(179, 166);
+            this.textBox1.Size = new System.Drawing.Size(297, 166);
             this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Местоположение рабочей папки программы: C:\\!invRep";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(305, 227);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "aida2csv";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- aida2csv/Form1.cs
+++ aida2csv/Form1.cs
@@ -1,32 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Security;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace aida2csv
 {
     public partial class Form1 : Form
     {
-        private object openFileDialog1;
-
         public Form1()
         {
             InitializeComponent();
         }
         //функция поиска в тексте
         public string textfromtag(string source, string begino, string finalo)
-        {
+        {//функция получения данных из тегов
             string[] res = { };
             int i = 0;
-
             finalo.Replace("/", @"\/");
             /////////
             //Regex Reg = new Regex(@"""" + begino + ""(.*?)"" + finalo+"""");
@@ -45,26 +36,43 @@ namespace aida2csv
                 i++; textBox1.Text += i.ToString();
            //     textBox7.Text += i.ToString() + "=";
             }
-            /////////
-           
+            /////////        
             return res[0];
         }
         //
         private void button1_Click(object sender, EventArgs e)
         {//получаем список полей для вібора из хтмл
-
-            string path = @"c:\!\";
-            string[] tab0 = File.ReadAllLines(path+"words.txt", Encoding.UTF8);
-
+            int flag = 0;
+            string path = @"c:\!invRep\";
+            string str="", path_words=path+"words.txt";
+            string[] str_ar_file = { "Кабинет","Имя комп","Имя польз.инв.","Перифирия","Лицензия","Корпус" };
+            string[] str_ar = {"Операционная система", "Пакет обновления ОС", "Имя компьютера","Имя пользователя", "Вход в домен","Тип ЦП", "Системная плата","Системная память", "Имя монитора", "ID монитора", "Дисковый накопитель", "Аппаратный адрес", "IP / маска подсети", "Шлюз", "Имя принтера" };
+            //string[] tab0 = File.ReadAllLines(path+"words.txt", Encoding.UTF8);
             //using (
             //FileStream fs = File.Open(path + "out.csv",FileMode.Create);
-            FileStream fs = File.Open(path + "out.csv", FileMode.Append);
-            //  )
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            //если файла words.txt нет, то создаем свой по умолчанию
+            if (!File.Exists(path_words))
+            {   FileStream fs_txt = File.Open(path_words, FileMode.Append);
+                using (StreamWriter sw_txt = new StreamWriter(fs_txt, Encoding.GetEncoding("Windows-1251")))
+                {for(int i=0;i<str_ar.Length;i++)
+                        sw_txt.WriteLine(str_ar[i]);}
+            }
+            //считываем поля из words.txt
+            string[] tab0 = File.ReadAllLines(path_words, Encoding.Default);
+            if (!File.Exists(path + "out.csv")) flag = 1;
+                FileStream fs = File.Open(path + "out.csv", FileMode.Append);
+                        //  )
             //{
-            StreamWriter sw = new StreamWriter(fs,Encoding.GetEncoding("Windows-1251"));
-            
-            string str = "Файл,Операционная система, Пакет обновления ОС, Имя компьютера,Имя пользователя, SMTP -адрес e - mail,Вход в домен,Тип ЦП, Системная плата,Системная память, Монитор, Дисковый накопитель,Первичный адрес IP,Первичный адрес MAC";
-            sw.WriteLine(str);
+            StreamWriter sw = new StreamWriter(fs,Encoding.GetEncoding("Windows-1251"));            
+            if (flag == 1)//если файла вообще небыло - формируем заголовок
+            {
+                for (int i = 0; i < str_ar_file.Length; i++) str += str_ar_file[i] + ";";                
+                for (int i = 0; i < str_ar.Length; i++) str += str_ar[i]+";";
+                sw.WriteLine(str);
+            }
+            //str = "Файл; Операционная система; Пакет обновления ОС; Имя компьютера;Имя пользователя; SMTP -адрес e - mail;Вход в домен;Тип ЦП; Системная плата;Системная память; Монитор; Дисковый накопитель;Первичный адрес IP;Первичный адрес MAC";
+            //sw.WriteLine(str);
             //}
 
             //получаем список файлов и обрабатіваем их
@@ -78,75 +86,87 @@ namespace aida2csv
             openFileDialog1.InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
            
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                
+            {                
                 string a = "";
                foreach (String file in openFileDialog1.FileNames)
                 {
                     a += file+ Environment.NewLine;
                     string[] file_html = File.ReadAllLines(file,Encoding.GetEncoding(1251));
+                    //читаем содержимое файла
                     try
-                    {
-                        sw.Write(file);
-                        foreach(string word_n in tab0)
-                        {
-                            sw.Write(",");
-                            //string file_body = "";
-                            
+                    {   //sw.Write(file);//вместо єтого расшифровать имя файла в стобцы
+                        string[] data_s = file.Split('~');
+                        //из имени файла извлекаем номер кабинета
+                        int n_slash = data_s[0].LastIndexOf(@"\");
+                        data_s[0] = data_s[0].Substring(n_slash+1, data_s[0].Length - n_slash-1);
+                        str = "";
+                        for (int i = 0; i < data_s.Length; i++) str += data_s[i] + ";";
+                        sw.Write(str);
+                        foreach (string word_n in tab0)
+                        {//перебор ключевых слов их таблицы
+                            //sw.Write(",");
+                            //sw.Write(";");
+                            //string file_body = "";                          
                             //
-                       
-                                foreach (string file_body in file_html)
-                                {
-                                // sw.Write(","+file_body);
-                                //string res = textfromtag(file_body, word_n, Environment.NewLine);
-                                //  textBox1.Text+= res;
-                                //string res = Regex.Match(file_body, word_n+@"(.*?| (\s ?|\S ?))"+Environment.NewLine).ToString();
-                               // string file_body0 = Regex.Replace(file_body, "<[^>]+>", string.Empty);
-                                int begn = file_body.IndexOf(word_n);
+                            foreach (string file_body in file_html)
+                                {//читаем строку из файла-отчета
+                                 // sw.Write(","+file_body);
+                                 //string res = textfromtag(file_body, word_n, Environment.NewLine);
+                                 //  textBox1.Text+= res;
+                                 //string res = Regex.Match(file_body, word_n+@"(.*?| (\s ?|\S ?))"+Environment.NewLine).ToString();
+                                 // string file_body0 = Regex.Replace(file_body, "<[^>]+>", string.Empty);
+                                string word_n2 = word_n + "&nbsp;&nbsp;";
+                                int begn = file_body.IndexOf(word_n2);
+                                //int begn = file_body.LastIndexOf(word_n);
                                 int ends = file_body.Length;
 
                                 if (begn > 0)
                                 {
-                                    
                                     string res = file_body.Substring(begn, ends-begn);
+                                //    begn = res.LastIndexOf(word_n);
+                                  //  ends = res.Length;
+                                    //res = res.Substring(begn, ends - begn);
+                                    //if (res.IndexOf(word_n) > 2) res = file_body.Substring(res.IndexOf(word_n), ends - begn);
+                                    
+                                    //MessageBox.Show(res.LastIndexOf(word_n).ToString());
                                     res = Regex.Replace(res, "<[^>]+>", string.Empty);
                                     res = Regex.Replace(res, ",", " ");
                                     begn = res.LastIndexOf("&nbsp;&nbsp;")+ ("&nbsp;&nbsp;").Length;
                                     ends = res.Length;
                                     res = res.Substring(begn, ends - begn);
+                                    res = Regex.Replace(res, "&nbsp;", "");
+                                    res = Regex.Replace(res, "&gt;", "");
+                                    res = Regex.Replace(res, "&lt;", "");
                                     //res.Replace(word_n, "_");
                                     //sw.Write("," +res.ToString());
+                                   // MessageBox.Show(res + "=" + word_n);
                                     sw.Write(res.ToString());
                                     break;
-                                }
-                                }
-                            
-                            
-                            //
-
-                            
+                                }}
+                            sw.Write(";"); //                            
                         }
-
-
                         sw.WriteLine();
-                        //  PictureBox pb = new PictureBox();
-                        //  Image loadedImage = Image.FromFile(file);
-                        //  pb.Height = loadedImage.Height;
-                        //   pb.Width = loadedImage.Width;
-                        //    pb.Image = loadedImage;
-                        //  flowLayoutPanel1.Controls.Add(pb);
                     }
                     catch (Exception ex)
-                    {
-                        // Could not load the image - probably related to Windows file system permissions.
-                        //      MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
-                        //        + ". You may not have permission to read the file, or " +
-                        //       "it may be corrupt.\n\nReported error: " + ex.Message);
+                    {//тут можем обработать вывод об ощибке открытия                       
+                        MessageBox.Show(ex.Message);
                     }
                 }
                 textBox1.Text+= a;
                 sw.Close();
+                //открываем папку с результами
+                   Process.Start(path);
+                //или так чтобы папка должна быть выбрана в проводнике....
+                //Process PrFolder = new Process();
+                //ProcessStartInfo psi = new ProcessStartInfo();
+                //psi.CreateNoWindow = true;
+                //psi.WindowStyle = ProcessWindowStyle.Normal;
+                //psi.FileName = "explorer";
+                //psi.Arguments = @"/n, /select, " + path;
+                //PrFolder.StartInfo = psi;
+                //PrFolder.Start();
             }
-        }
+            Application.Exit();
+        }        
     }
 }
