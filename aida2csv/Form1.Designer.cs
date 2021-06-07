f7fbbf9d29eb6e866c043ffb63a1d0f01bcc19ba
Ol Kovalenko <avk013@gmail.com>
2021-06-07 05:20:13 +0300

    v3, на тестирование

--- aida2csv/Form1.Designer.cs
+++ aida2csv/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -48,7 +49,7 @@
             this.textBox1.Location = new System.Drawing.Point(5, 24);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(297, 166);
+            this.textBox1.Size = new System.Drawing.Size(321, 166);
             this.textBox1.TabIndex = 1;
             // 
             // label1
@@ -60,16 +61,26 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Местоположение рабочей папки программы: C:\\!invRep";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "(а) avk013 \"УДП\" 2021";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 227);
+            this.ClientSize = new System.Drawing.Size(330, 227);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "aida2csv";
+            this.Text = "aida2csv (v3)";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +91,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
